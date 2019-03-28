259f42f3f455f764bbb1afa49bb2ebc0db78d01f
Nathan Stackpole <nstackpole21@gmail.com>
2019-03-27 20:39:32 -0400

    Now uses pygame

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,18 +1,28 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import random
 import argparse
 import pygame
+import sys
+
+pygame.init()
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
+ALIVE = [255,0,32]
+DEAD = [0,255,255]
+ 
+# This sets the margin between each cell
+MARGIN = 1
 
 def add_glider(i, j, grid):
     glider = np.array([[0,0,255], [255,0,255], [0,255,255]])
     grid[i:i+3, j:j+3] = glider
 
 def random_grid(N):
-    return np.random.choice([255, 0], N*N, p=[0.1, 0.9]).reshape(N, N)
+    return np.random.choice([255, 0], N*N, p=[0.2, 0.8]).reshape(N, N)
 
-def update(frameNum, img, grid, N):
+def update(grid, N, screen):
     # copy grid since we require 8 neighbors for calculation
     # and we go line by line
     newGrid = grid.copy()
@@ -27,15 +37,15 @@ def update(frameNum, img, grid, N):
             if grid[i, j] == 255:
                 if (total < 2) or (total > 3):
                     newGrid[i, j] = 0
+                    pygame.draw.rect(screen, DEAD, [(MARGIN + WIDTH) * j + MARGIN, (MARGIN + HEIGHT) * i + MARGIN, WIDTH, HEIGHT])
             else:
                 if total == 3:
                     newGrid[i, j] = 255
+                    pygame.draw.rect(screen, ALIVE, [(MARGIN + WIDTH) * j + MARGIN, (MARGIN + HEIGHT) * i + MARGIN, WIDTH, HEIGHT])
+
 
     # update data
-    img.set_data(newGrid)
     grid[:] = newGrid[:]
-    return img,
-    
 
 def main():
     parser = argparse.ArgumentParser(description ="Runs Conway's Game of Life simulation")
@@ -62,15 +72,21 @@ def main():
     else:
         grid = random_grid(N)
 
-    fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation='nearest')
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ), frames=1000, interval = update_interval, save_count = 50)
-
-    if args.movfile:
-        ani.save(args.movfile, fps=30, extra_args=['vcodec', 'libx264]'])
+    pygame.display.set_caption("The Game of Life")
+    screen = pygame.display.set_mode([700, 700])
+    screen.fill([0,0,0])
+    clock = pygame.time.Clock()
+    not_done = True
+    while not_done:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                not_done = True  # Flag that we are done so we exit this loop
+        clock.tick(update_interval)
+        update(grid, N, screen)
+        pygame.display.flip()
+    pygame.display.quit()
+    pygame.quit()
+    sys.exit()
     
-    # plt.imshow(grid, interpolation='nearest')
-    plt.show()
-
 
 main()
